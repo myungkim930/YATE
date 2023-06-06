71c23907b7c64aa0d91002a297cfe940766614b2
myungkim930 <myung.kim@inria.fr>
2023-06-06 21:27:20 +0200

    delete folder

--- loss/__init__.py
+++
@@ -1,9 +0,0 @@
-from .infonce import Infonce_loss
-from .utils import create_target_node
-
-__all__ = [
-    "Infonce_loss",
-    "create_target_node",
-]
-
-classes = __all__
--- train/__init__.py
+++
@@ -1,8 +0,0 @@
-from .utils import CosineAnnealingWarmUpRestarts, Index_extractor
-
-__all__ = [
-    "CosineAnnealingWarmUpRestarts",
-    "Index_extractor",
-]
-
-classes = __all__
